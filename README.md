1e80a9d9522c4d4b40f431b9004c4fabd8b788b0
Anuj Patel <indyaah@users.noreply.github.com>
2018-04-08 00:23:58 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+motrics : (Mock + Metrics)
+===
---
+++ src/main/java/pro/anuj/challenge/motrics/MotricsApp.java
@@ -0,0 +1,13 @@
+package pro.anuj.challenge.motrics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MotricsApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MotricsApp.class);
+    }
+
+}
